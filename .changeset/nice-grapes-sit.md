fe4e29dfc8f670988679c98c2043cc3e40092808
memo.repo <1r56955415918@qq.com>
2023-12-24 21:37:31 +0800

    :bug:fix(uni-configuration-parsing): 修复安卓默认权限配置丢失

---
+++ .changeset/nice-grapes-sit.md
@@ -0,0 +1,5 @@
+---
+"@memo28.cmd/uni-configuration-parsing": patch
+---
+
+修复安卓默认权限配置丢失
--- packages/uniConfigurationParsing/src/features/parsingConfiguration/app/distribute/permissions.ts
+++ packages/uniConfigurationParsing/src/features/parsingConfiguration/app/distribute/permissions.ts
@@ -1,22 +1,23 @@
 /*
  * @Author: @memo28.repo
  * @Date: 2023-12-22 15:55:25
- * @LastEditTime: 2023-12-23 14:18:41
+ * @LastEditTime: 2023-12-24 21:36:19
  * @Description: 
  * @FilePath: /cmdRepo/packages/uniConfigurationParsing/src/features/parsingConfiguration/app/distribute/permissions.ts
  */
+import { androidDefaultPermissions } from '../../../configuration/app/distribute/index';
 
 
 
 export class Permissions {
-    private permissions: string[] = [];
+    private permissions: string[] = [...androidDefaultPermissions];
 
     setPermissions(permissions: string[]) {
         this.permissions = permissions
         return this
     }
 
-    push(permissions: string[]) { 
+    push(permissions: string[]) {
         this.permissions.push(...permissions)
         return this
     }
